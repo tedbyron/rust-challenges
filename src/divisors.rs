f1d9515f4023421b5667198b6a75bbd72d0264fe
Teddy Byron <ted@tedbyron.com>
2020-03-03 00:34:40 -0500

    Add comments to divisors.rs

--- src/divisors.rs
+++ src/divisors.rs
@@ -1,3 +1,5 @@
+// get all the divisors from 2 to an integer, or else return a string saying the
+// integer is prime
 pub fn divisors(integer: u32) -> Result<Vec<u32>, String> {
     let divs = (2..integer)
         .filter(|i| integer % i == 0)
