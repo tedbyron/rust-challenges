09dc9968bb9f11225b52c4f42d5a75935daa7a45
Teddy Byron <ted@tedbyron.com>
2020-03-11 17:32:58 -0400

    Update molecule_to_atom.rs

--- src/_2020/molecule_to_atoms.rs
+++ src/_2020/molecule_to_atoms.rs
@@ -9,11 +9,69 @@ type Molecule = Vec<Atom>;
 #[derive(Debug)]
 pub struct ParseError {}
 
-fn get_components(s: &str) -> Result<Vec<String>, ParseError> {
-    let parts = Regex::new(r"[A-Z]{1}[a-z]?\d*|[\(\[\{]|[\)\]\}]\d*").unwrap();
-    Err(ParseError {})
+fn has_balanced_parens(s: &str) -> bool {
+    let paren_pattern = Regex::new(r"[\(\)\[\]\{\}]").unwrap();
+    let parens = paren_pattern
+        .find_iter(s)
+        .map(|m| m.as_str())
+        .collect::<Vec<&str>>();
+
+    let (parens_l, parens_r) = parens.split_at(parens.len() / 2);
+
+    if parens_r
+        .iter()
+        .rev()
+        .enumerate()
+        .any(|(i, slice)| match slice {
+            &")" => parens_l[i] != "(",
+            &"]" => parens_l[i] != "[",
+            &"}" => parens_l[i] != "{",
+            _ => true,
+        })
+    {
+        return false;
+    }
+
+    true
 }
 
+fn get_subcomponents(r: &Regex, s: &str) -> Option<Vec<String>> {
+    let subcomponents = r
+        .find_iter(s)
+        .map(|m| m.as_str().to_string())
+        .collect::<Vec<String>>();
+
+    if subcomponents.len() > 0 {
+        Some(subcomponents)
+    } else {
+        None
+    }
+}
+
+fn get_components(s: &str) -> Result<Vec<String>, ParseError> {
+    if !has_balanced_parens(s) {
+        return Err(ParseError {});
+    }
+
+    let component_pattern = Regex::new(r"([A-Z][a-z]?\d*)+|[\(\[\{]|[\)\]\}]\d*").unwrap();
+    match get_subcomponents(&component_pattern, s) {
+        Some(c) => Ok(c),
+        None => Err(ParseError {}),
+    }
+}
+
+// fn get_multiplier(r: &Regex, s: &str) -> i32 {
+//     0
+// }
+
 pub fn parse_molecule(s: &str) -> Result<Molecule, ParseError> {
+    let components = get_components(s)?;
+    let subcomponent_pattern = Regex::new(r"[A-Z][a-z]?\d*").unwrap();
+
+    println!(
+        "{:?}",
+        get_subcomponents(&subcomponent_pattern, &components[4]) // components
+    );
+
     Err(ParseError {})
 }
--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,9 @@ mod _2020;
 use _2020::molecule_to_atoms::parse_molecule;
 
 fn main() {
-    println!("{:?}", parse_molecule("K4[ON(SO3)2]2"));
+    println!("{:?}", parse_molecule("pie"));
     println!("{:?}", parse_molecule("Mg(OH"));
     println!("{:?}", parse_molecule("Mg)OH("));
     println!("{:?}", parse_molecule("Mg(OH}2"));
+    println!("{:?}", parse_molecule("K4[ON(SOx3)2]2"));
 }
