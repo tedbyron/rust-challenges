69d27caecd8805dfaeebfba0e374a4248ff679bb
Teddy Byron <ted@tedbyron.com>
2020-03-11 17:59:15 -0400

    Add comments to molecule_to_atom.rs

--- src/_2020/molecule_to_atoms.rs
+++ src/_2020/molecule_to_atoms.rs
@@ -9,7 +9,8 @@ type Molecule = Vec<Atom>;
 #[derive(Debug)]
 pub struct ParseError {}
 
-fn has_balanced_parens(s: &str) -> bool {
+/// check if a string slice has balanced parentheses
+fn is_balanced(s: &str) -> bool {
     let paren_pattern = Regex::new(r"[\(\)\[\]\{\}]").unwrap();
     let parens = paren_pattern
         .find_iter(s)
@@ -35,7 +36,8 @@ fn has_balanced_parens(s: &str) -> bool {
     true
 }
 
-fn get_subcomponents(r: &Regex, s: &str) -> Option<Vec<String>> {
+/// using a compiled regular expression, get its matches from a string slice
+fn get_matches(r: &Regex, s: &str) -> Option<Vec<String>> {
     let subcomponents = r
         .find_iter(s)
         .map(|m| m.as_str().to_string())
@@ -48,30 +50,26 @@ fn get_subcomponents(r: &Regex, s: &str) -> Option<Vec<String>> {
     }
 }
 
+/// get the components of a chemical formula in the form of a string slice
 fn get_components(s: &str) -> Result<Vec<String>, ParseError> {
-    if !has_balanced_parens(s) {
+    if !is_balanced(s) {
         return Err(ParseError {});
     }
 
-    let component_pattern = Regex::new(r"([A-Z][a-z]?\d*)+|[\(\[\{]|[\)\]\}]\d*").unwrap();
-    match get_subcomponents(&component_pattern, s) {
+    match get_matches(
+        &Regex::new(r"([A-Z][a-z]?\d*)+|[\(\[\{]|[\)\]\}]\d*").unwrap(),
+        s,
+    ) {
         Some(c) => Ok(c),
         None => Err(ParseError {}),
     }
 }
 
-// fn get_multiplier(r: &Regex, s: &str) -> i32 {
-//     0
-// }
-
 pub fn parse_molecule(s: &str) -> Result<Molecule, ParseError> {
     let components = get_components(s)?;
     let subcomponent_pattern = Regex::new(r"[A-Z][a-z]?\d*").unwrap();
 
-    println!(
-        "{:?}",
-        get_subcomponents(&subcomponent_pattern, &components[4]) // components
-    );
+    println!("{:?}", get_matches(&subcomponent_pattern, &components[4]));
 
     Err(ParseError {})
 }
