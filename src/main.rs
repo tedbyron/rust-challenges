7f755300aad80c34fed4a027dff7e33781b9c0cd
Teddy Byron <ted@tedbyron.com>
2021-08-27 22:50:05 -0400

    update lockfile

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,7 @@ mod strings;
 use math::primes::nth_prime::nth;
 
 fn main() {
-    println!("{}", nth(0));
-    println!("{}", nth(1));
-    println!("{}", nth(2));
-    println!("{}", nth(5));
+    for i in 0..1000 {
+        println!("{}", nth(i));
+    }
 }
